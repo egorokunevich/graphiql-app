4d1fb7e401b737ad53c4d8f3b48b9702ed34ebf1
egorokunevich <84414222+egorokunevich@users.noreply.github.com>
2024-08-30 00:00:58 +0300

    Fix assets (#8)
    * fix: assets not loading properly

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,19 @@ import type { NextRequest } from 'next/server';
 import { i18n } from './i18n-config';
 import { LanguageType } from './src/components/LanguageToggle/LanguageToggle';
 
+const PUBLIC_FILE = /\.(.*)$/;
+
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
+
+  if (
+    request.nextUrl.pathname.startsWith('/_next') ||
+    request.nextUrl.pathname.includes('/api/') ||
+    request.nextUrl.pathname.includes('/static/') ||
+    PUBLIC_FILE.test(request.nextUrl.pathname)
+  ) {
+    return;
+  }
 
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
@@ -25,8 +36,3 @@ export function middleware(request: NextRequest) {
     );
   }
 }
-
-export const config = {
-  // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-};
