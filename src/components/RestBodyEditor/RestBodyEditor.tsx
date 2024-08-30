2bc70ab16ce8cfd3b7560a7c590ae1d0a01e9033
manzura94 <manzura_jabborova@mail.ru>
2024-08-30 22:25:51 +0300

    feat: add body url

--- src/components/RestBodyEditor/RestBodyEditor.tsx
+++ src/components/RestBodyEditor/RestBodyEditor.tsx
@@ -1,5 +1,4 @@
 import MonacoEditor from '@monaco-editor/react';
-import EditorDidMount from '@monaco-editor/react';
 import {
   Box,
   Radio,
@@ -7,12 +6,10 @@ import {
   FormControlLabel,
   RadioGroup,
 } from '@mui/material';
-import parserBabel from 'prettier/parser-babel';
-import prettier from 'prettier/standalone';
 import React, {
   Dispatch,
   SetStateAction,
-  useCallback,
+  useEffect,
   useRef,
   useState,
 } from 'react';
@@ -24,7 +21,7 @@ interface RestBodyEditorProps {
 
 const RestBodyEditor: React.FC<RestBodyEditorProps> = ({ body, setBody }) => {
   const [language, setLanguage] = useState<string>('json');
-  const editorRef = useRef(null);
+  const previousBodyRef = useRef<string>(body);
 
   const handleChange = (value: string | undefined) => {
     if (value !== undefined) {
@@ -35,6 +32,47 @@ const RestBodyEditor: React.FC<RestBodyEditorProps> = ({ body, setBody }) => {
   const handleLanguageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setLanguage(event.target.value);
   };
+
+  const updateUrlWithEncodedBody = () => {
+    try {
+      let updatedBody = body;
+
+      if (language === 'json') {
+        const parsed = JSON.parse(body);
+        updatedBody = JSON.stringify(parsed, null, 2);
+      }
+      if (updatedBody !== previousBodyRef.current) {
+        const base64Body = btoa(updatedBody);
+
+        const url = new URL(window.location.href);
+        url.pathname = `${url.pathname.split('/').slice(0, -1).join('/')}/${base64Body}`;
+        window.history.pushState({}, '', url.toString());
+
+        previousBodyRef.current = updatedBody;
+      }
+    } catch (error) {
+      console.error('Failed to parse JSON or update URL:', error);
+    }
+  };
+
+  useEffect(() => {
+    const handleFocusOut = (event: FocusEvent) => {
+      if (
+        event.relatedTarget === null ||
+        !(
+          event.relatedTarget instanceof Node &&
+          document.contains(event.relatedTarget)
+        )
+      ) {
+        updateUrlWithEncodedBody();
+      }
+    };
+
+    window.addEventListener('focusout', handleFocusOut);
+    return () => {
+      window.removeEventListener('focusout', handleFocusOut);
+    };
+  }, [body, language]);
 
   return (
     <Box sx={{ padding: 1, backgroundColor: '#F0F7F4', borderRadius: 1 }}>
@@ -66,8 +104,6 @@ const RestBodyEditor: React.FC<RestBodyEditorProps> = ({ body, setBody }) => {
         language={language === 'json' ? 'json' : 'plaintext'}
         value={body}
         onChange={handleChange}
-        // editorDidMount={editorDidMount}
-        // onBlur={handleBlur}
         theme="vs-white"
       />
     </Box>
