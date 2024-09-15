c722153988e115443c2a1303e3bae605a640abf5
egorokunevich <mrcraigre@gmail.com>
2024-09-16 00:27:30 +0300

    feat: add layout tests

--- src/tests/RootLayout.test.tsx
+++ src/tests/RootLayout.test.tsx
@@ -11,6 +11,7 @@ import {
 } from './mocks/mockFirebase';
 
 import { mockPush } from '@/setupJest';
+import ApiLayout from '@app/api/layout';
 import LangLayout from '@src/components/LangLayout/LangLayout';
 import { render } from '@src/tests/test-utils';
 
@@ -35,6 +36,7 @@ jest.mock('firebase/auth', () => {
 describe('RootLayout', () => {
   it('Should render in the document', async () => {
     const child = <div></div>;
+
     const langLayout = await LangLayout({
       children: child,
     });
@@ -55,11 +57,29 @@ describe('RootLayout', () => {
 
     render(langLayout);
 
+    const layout = await screen.findByTestId('lang-layout');
+    expect(layout).toBeInTheDocument();
+
     const signInBtn = await screen.findByTestId('btn-signIn');
     expect(signInBtn).toBeInTheDocument();
 
     await user.click(signInBtn);
 
     expect(mockPush).toHaveBeenCalledWith('/en/authorization');
+  });
+});
+
+describe('ApiLayout', () => {
+  it('Should render in the document', async () => {
+    const child = <div></div>;
+
+    const apiLayout = await ApiLayout({
+      children: child,
+    });
+
+    render(apiLayout);
+
+    const layout = await screen.findByTestId('api-layout');
+    expect(layout).toBeInTheDocument();
   });
 });
